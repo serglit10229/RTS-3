5cb36219c8ab17470609c12f5b385df45bf9b1ef
serglit1650 <direct.serglit@gmail.com>
2018-02-06 15:12:26 -0800

    Attacking

---
+++ Assets/Destroy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour {
+
+	public string enemyTag;
+
+	void OnTriggerEnter(Collider col){
+		
+		if(col.gameObject.tag == enemyTag)
+		{
+			//yield WaitForSeconds(delay);
+			Destroy (gameObject);
+			Debug.Log("Destroy");
+		}
+	}
+}
